072de2efb34c3fcc4bb8494ad6b97c0a3e66d3f5
Venkataramanareactdev <venkataramanag890@gmail.com>
2025-09-30 10:41:20 +0530

    changes in buttons jsx

--- src/useState_examples/Buttons.jsx
+++ src/useState_examples/Buttons.jsx
@@ -4,15 +4,11 @@ const Buttons = () => {
   const[count, setcount]= useState(0)
   return (
     <div>
-        <p>Count:{count}</p>
+      <p>Count:{count}</p>
       <button onClick={()=>setcount(count + 1) }> increase</button>
       <button onClick={()=>count > 0 && setcount(count - 1) }>decrease</button>
       <button onClick={()=>setcount(0) }>Reset</button>
-
     </div>
-  
   )
-  
-}
-
+  }
 export default Buttons
